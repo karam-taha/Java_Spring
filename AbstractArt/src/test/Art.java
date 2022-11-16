70fcc4f632f806252d66f2c574e640529652ddb8
Karam Taha <karamtaha15@gmail.com>
2022-11-16 13:17:46 +0200

    Abstract Art assignment in java OOP

---
+++ AbstractArt/src/test/Art.java
@@ -0,0 +1,9 @@
+package test;
+
+public abstract class Art {
+	private String title;
+	private String author;
+	private String description;
+	public abstract void viewArt();
+	
+}
---
+++ AbstractArt/src/test/Painting.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Painting extends Art {
+	private String paintType;
+	public Painting(String paintType) {
+		super();
+		this.paintType = paintType;
+	}
+	public String getPaintType() {
+		return paintType;
+	}
+	public void setPaintType(String paintType) {
+		this.paintType = paintType;
+	}
+	public void viewArt() {
+		System.out.println("Info about the art work: " + this.paintType);
+
+	}
+
+}
